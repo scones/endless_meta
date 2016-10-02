eb0644f3d510c92a92a5ac8d3777c3e295c538f4
Dirk Gustke <code@asm68k.org>
2016-10-02 13:01:35 +0200

    master - more test files

---
+++ tests/src/core/environment/json_load_test.cpp
@@ -0,0 +1,50 @@
+/*
+ * json_load_test.cpp
+ *
+ *  Created on: 2 Oct 2016
+ *      Author: scones
+ */
+
+
+#define BOOST_TEST_DYN_LINK
+#ifdef STAND_ALONE
+#   define BOOST_TEST_MODULE Main
+#endif
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_SUITE(json_load_test)
+
+
+BOOST_AUTO_TEST_CASE(json_load_int) {
+
+}
+
+
+BOOST_AUTO_TEST_CASE(json_load_double) {
+
+}
+
+
+BOOST_AUTO_TEST_CASE(json_load_boolean) {
+
+}
+
+
+BOOST_AUTO_TEST_CASE(json_load_string) {
+
+}
+
+
+BOOST_AUTO_TEST_CASE(json_load_array) {
+
+}
+
+
+BOOST_AUTO_TEST_CASE(json_load_object) {
+
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
---
+++ tests/src/core/meta/operator_equal_test.cpp
@@ -0,0 +1,32 @@
+/*
+ * operator_equal_test.cpp
+ *
+ *  Created on: 2 Oct 2016
+ *      Author: scones
+ */
+
+
+#define BOOST_TEST_DYN_LINK
+#ifdef STAND_ALONE
+#   define BOOST_TEST_MODULE Main
+#endif
+#include <boost/test/unit_test.hpp>
+
+
+#include "core/meta.h"
+
+/*
+BOOST_AUTO_TEST_SUITE(operator_equal_test)
+
+
+BOOST_AUTO_TEST_CASE(compare_int) {
+//  core::meta a(1);
+//  core::meta b(1);
+
+//  BOOST_CHECK(a == b);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+*/
